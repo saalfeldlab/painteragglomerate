2df8828e1efb1d98eb0e3be0a4dcd941c8abeadb
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2018-07-12 18:02:44 -0400

    Add command line arg parser

--- src/main/kotlin/org/janelia/saalfeldlab/paintera/PainterAgglomerate.kt
+++ src/main/kotlin/org/janelia/saalfeldlab/paintera/PainterAgglomerate.kt
@@ -2,11 +2,29 @@ package org.janelia.saalfeldlab.paintera
 
 import bdv.viewer.ViewerOptions
 import javafx.application.Application
+import javafx.application.Platform
 import javafx.scene.Scene
 import javafx.stage.Stage
+import picocli.CommandLine
+import java.util.*
 
 class PainterAgglomerate : Application() {
+
+
     override fun start(primaryStage: Stage?) {
+
+
+        val args = parameters.getRaw().toTypedArray();
+
+        val cmdLineArgs = CmdLineArgs()
+        val parsedSuccessfully = Optional.ofNullable(CommandLine.call(cmdLineArgs, System.err, *args)).orElse(false)
+
+        if (!parsedSuccessfully) {
+            return
+        }
+
+        Platform.setImplicitExit(true)
+
         val pbv = PainteraBaseView(1, ViewerOptions.options())
         val scene = Scene(pbv.pane(), 800.0, 600.0)
         primaryStage!!.scene = scene
