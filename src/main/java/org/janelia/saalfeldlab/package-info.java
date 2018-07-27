ed992924a0eb3b156f30f557b6bbd9073798ddf0
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2018-07-27 17:48:13 -0400

    Remove unused classes

--- src/main/java/org/janelia/saalfeldlab/package-info.java
+++
@@ -1,1 +0,0 @@
-package org.janelia.saalfeldlab;
--- src/main/kotlin/org/janelia/saalfeldlab/paintera/solver/AutomaticSolutionUpdateFromServer.kt
+++
@@ -1,9 +0,0 @@
-package org.janelia.saalfeldlab.paintera.solver
-
-import gnu.trove.map.hash.TLongLongHashMap
-
-interface AutomaticSolutionUpdateFromServer {
-
-    fun latestSolution(): TLongLongHashMap
-
-}
