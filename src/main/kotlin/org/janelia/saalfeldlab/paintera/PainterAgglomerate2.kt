1290a38eabf93c67a4dd878174e63ab5772244e3
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2019-03-07 14:39:24 -0500

    Simplify to args.none

--- src/main/kotlin/org/janelia/saalfeldlab/paintera/PainterAgglomerate2.kt
+++ src/main/kotlin/org/janelia/saalfeldlab/paintera/PainterAgglomerate2.kt
@@ -7,7 +7,7 @@ class PainterAgglomerate2 {
     companion object {
 
         fun addTempDirArg(args: Array<String>): Array<String> {
-            return if (args.filter { it == "--default-to-temp-directory" }.isEmpty())
+            return if (args.none { it == "--default-to-temp-directory" })
                 args + arrayOf("--default-to-temp-directory") else
                 args
         }
