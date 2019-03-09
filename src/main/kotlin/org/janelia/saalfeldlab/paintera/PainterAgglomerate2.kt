8488cebb237836527c90b031e05e3aa1694c39e8
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2019-03-09 09:35:17 -0500

    Log warning to user that class is only for test purposes

--- src/main/kotlin/org/janelia/saalfeldlab/paintera/PainterAgglomerate2.kt
+++ src/main/kotlin/org/janelia/saalfeldlab/paintera/PainterAgglomerate2.kt
@@ -1,6 +1,8 @@
 package org.janelia.saalfeldlab.paintera
 
 import javafx.application.Application
+import org.slf4j.LoggerFactory
+import java.lang.invoke.MethodHandles
 
 class PainterAgglomerate2 {
 
@@ -14,7 +16,10 @@ class PainterAgglomerate2 {
     }
 }
 
+private val LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass())
+
 fun main(args: Array<String>) {
+    LOG.warn("This is just a helper class for testing! For real application, run plain Paintera and add this jar with dependencies to classpath.")
     Application.launch(Paintera::class.java, *PainterAgglomerate2.addTempDirArg(args))
 }
 
